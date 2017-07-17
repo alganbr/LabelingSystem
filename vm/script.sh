bc60a68ec4b7a338f646a102e7b373b8be8431e5
alganbr <algan_br@hotmail.com>
2017-07-17 15:56:38 -0700

    Initial settings

---
+++ vm/script.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y apache2 python-pip python-dev libpq-dev postgresql postgresql-contrib
+
+rm -rf /var/www
+ln -fs /vagrant /var/www
+
+# Install python requirements
+pip install -r /home/vagrant/labelingsystem/requirements.txt
+
+# Create database and syncdb
+echo "ALTER USER postgres PASSWORD 'postgres'" | sudo -u postgres psql
+pushd /home/vagrant/labelingsystem
+/usr/bin/python manage.py migrate
+popd
+
+# Give everything in home folder back to vagrant user
+chown -R vagrant:vagrant /home/vagrant
