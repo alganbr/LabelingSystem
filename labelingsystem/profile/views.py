5f8f64a6e7fd3aa0c26269b9d88925d8ab931595
alganbr <algan_br@hotmail.com>
2017-08-06 21:10:28 -0700

    profile update form

--- labelingsystem/profile/views.py
+++ labelingsystem/profile/views.py
@@ -3,35 +3,57 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView, ListView
+from django.views.generic import CreateView, DetailView, UpdateView, ListView, FormView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 
-from . import models
-from . import forms
+from .models import Profile
+from .forms import UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+class ProfileUpdateView(LoginRequiredMixin, FormView):
 	model = User
 	template_name = 'profile/profile_update.html'
 
-class ProfileDetailView(DetailView):
+	def get_context_data(self, **kwargs):
+		context = super(ProfileUpdateView, self).get_context_data(**kwargs)
+		return context
+
+class ProfileDetailView(UpdateView):
 	model = User
 	template_name = 'profile/profile_detail.html'
+	fields = ('first_name', 'last_name')
 	slug_field = 'username'
 	slug_url_kwarg = 'username'
-	context_object_name = 'user'
 
 	def get_object(self):
 		return User.objects.get(username=self.kwargs.get('username'))
 
 	def get_context_data(self, **kwargs):
 		context = super(ProfileDetailView, self).get_context_data(**kwargs)
+		context["user_form"] = UserUpdateForm(self.request.POST or None, instance=self.get_object())
+		context["profile_form"] = ProfileUpdateForm(self.request.POST or None, self.request.FILES, instance=self.get_object().profile)
 		return context
+
+	def post(self, request, *args, **kwargs):
+		self.object = self.get_object()
+		context = self.get_context_data()
+		user_form = context['user_form']
+		profile_form = context['profile_form']
+		if user_form.is_valid():
+			user_form.save()
+
+		if profile_form.is_valid():
+			profile_form.save()
+
+		return super(ProfileDetailView, self).form_valid(profile_form)
+
+	def get_success_url(self, **kwargs):
+		return reverse_lazy('profile:profile_detail', kwargs={'username': self.request.user.username})
 
 class ProfileListView(ListView):
 	model = User
